84b65ab8987808397a3ff019fcdf37cf23bd11fb
Johanna Ljungberg <johanna.ljungberg2@learnet.se>
2021-04-25 15:48:26 +0200

    sparar ner översättnignen till Databasen. Använder Task<TResult> samt Async och await

--- RobberLanguageAPI/Controllers/TranslationController.cs
+++ RobberLanguageAPI/Controllers/TranslationController.cs
@@ -1,24 +1,30 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using RobberLanguageAPI.Data;
 using RobberLanguageAPI.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mime;
 using System.Threading.Tasks;
 
 namespace RobberLanguageAPI.Controllers
 {
   
+
     [ApiController]
     [Route("api/RobberLanguage")] // Här ändrar jag Route-attributet för min Controller
 
     //Controller : en klass som innehåller flera actions. Actions är något som händer i
     // ASP.NET Core programmet. Men här, istället för att retuerna en view, kan man se det som
     // om att varje action returnera ett json-objekt. Ett slags svar på ett web-Api-anrop.
+ 
     public class TranslationController : ControllerBase
     {
 
+        
+        
         /*
           Här skapar jag en metod som tar in en sträng och
           översätter den till Rövarspråket. Vanligtvis vill man inte lägga dessa
@@ -52,10 +58,12 @@ namespace RobberLanguageAPI.Controllers
         }
     
         private readonly ILogger<TranslationController> _logger;
+        private readonly RobberTranslationDBContext _context;
 
-        public TranslationController(ILogger<TranslationController> logger)
+        public TranslationController(ILogger<TranslationController> logger, RobberTranslationDBContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         //POST: api/RobberLanguage/CreateNewTranslation
@@ -65,15 +73,28 @@ namespace RobberLanguageAPI.Controllers
         /*Här skapar jag en POST-endpoint som returernar ett Translation-objekt. Detta objekt
          innehåller värden för OriginalSentence och TraslatedSentence.
         Nu kan man köra och testa programmet. */
-        public Translation createTranslation(Translation originalSentence)
+        public async Task <ActionResult<Translation>> createTranslation(Translation originalSentence)
         {
+
             var Translation = new Translation
             {
                 OriginalSentence = originalSentence.OriginalSentence,
                 TranslatedSentence = $"{TranslateSentence(originalSentence.OriginalSentence) }"
-
             };
+            
+            if (Translation == null)
+                {
+                return BadRequest();
+            }
+
+            /*
+             * Här sparar vi ner översättningen till Databasen.
+             */
+            await _context.Translations.AddAsync(Translation);
+            await _context.SaveChangesAsync();
+
             return Translation;
+
         }
 
 
